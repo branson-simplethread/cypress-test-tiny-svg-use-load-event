e634be83eea99b9c694896ddc6410032ccf6c249
Branson <branson@simplethread.com>
2024-06-20 09:25:26 -0500

    Add note

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
+    // NOTE: the scenario is located in an external document because
+    // "'file:' URLs are treated as unique security origins."
+    // and the browser refuses to load the SVG.
     cy.visit('https://branson-simplethread.github.io/svg-example/icons.html')
   })
 })
